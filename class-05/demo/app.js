e722cbf1cba271fa1d94423fe7491adcacdc76d7
baraahhawari <baraah.hawari@gmail.com>
2021-04-29 11:51:21 +0300

    demo

---
+++ class-05/demo/app.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/*
+Review for yesterday
+
+1. functions
+
+- function declaration
+getCity('Karak');
+
+function getCity(city) {
+    // console.log('I live in' + city);
+    console.log(`I live in ${city}`);
+}
+
+*/
+
+// function expression 
+// let getAge = function(year) {
+//     let output = 2021 - year;
+//     return alert(output);
+
+// }
+// getAge(2000);
+
+// anonymous function ==> doesn't have name
+
+// (function() {
+//     console.log('Hello function');
+// })();
+
+
+// Scopes
+// 1. Global [in the app.js not inside any scope for {loop, function ....}]
+// 2. Local {inside here}
+
+// let userName = 'Sultan';
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+//     let outPut = `Hello There ${userName}`;
+//     console.log('inside the for loop', outPut);
+// }
+
+// console.log(i); I cant do this
+// console.log('outside the for loop', outPut); I cant do this
+
+
+// Pair programming
+/*
+2 people working together
+Navigator: tha owner of the code, and the one who do the talk part
+Driver: who is do the coding part
+
+
+*/
